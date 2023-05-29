9a9f4dbf5427a907c25b68444aec6ef4d8306c48
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-29 14:28:10 -0300

    creating coffe section and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import { MapPin, ShoppingCart } from "phosphor-react";
-import { HeaderContainer } from "./style";
+import { Coffee, MapPin, Package, ShoppingCart, Timer } from "phosphor-react";
+import { HeaderContainer, IconsContainer, SectionContainer } from "./style";
 import "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <HeaderContainer>
-        <img src="./src/assets/coffe-logo.svg" alt="" />
-        <p>
-          <MapPin size={22} color="#4B2995" weight="fill" /> João Pessoa, PB 
-        </p>
-       <span>
-        <button><ShoppingCart size={22} color="#C47F17" weight="fill" /></button>
-       </span>
-      </HeaderContainer>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <HeaderContainer>
+          <img src="./src/assets/coffe-logo.svg" alt="" />
+          <p>
+            <MapPin size={22} color="#4B2995" weight="fill" /> João Pessoa, PB
+          </p>
+          <span>
+            <button>
+              <ShoppingCart size={22} color="#C47F17" weight="fill" />
+            </button>
+          </span>
+        </HeaderContainer>
+
+
+      <SectionContainer>
+        <div className="about-coffee-section">
+          <h1>
+            Encontre o café perfeito <br /> para qualquer hora do dia
+          </h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a <br />{" "}
+            qualquer hora
+          </p>
+          <img
+          src="https://s3-alpha-sig.figma.com/img/731e/f48d/acb13d1b7c718c6f523d5dc02407a226?Expires=1685923200&Signature=m2BPFQQgeohcIuk9CWCHIympI9pTjHRwQGa3vqrPhoRliggF1uhRf3ty1d6Clt8JanRp1W7r6Pla9jZL7TeBz~SRCKEK4uo9jiJnJEQzaBKSJqgqWgvmZgd8Bi00qxVxj-bG8AsWwjDRhOh9Zd2Pt1S-ZuV30S4YhuOwAYRGyS8TAT-6bsdyvbncZmC4NaLcU5md8Wc8q-ckyJ-G2saDx7C-nIIJ5RBFAb8munuiGDwIJH~7I2jt2~8v~7qkCwloDH8ciE1dvwR3f3qoE6btLgKK03JVfBM5W6360EEqAz9mPLIJkQ2sBVl6~Uz0McMPQKz7rHTGIxiIxuRNGeAV9w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          alt=""
+        />
+        </div>
+        <IconsContainer>
+          <div>
+            <ShoppingCart className="shoppingCart-icon" weight="fill" color="#ffff" /> Compra
+            simples e segura <Package  className="package-icon" weight="fill" color="#ffff" />{" "}
+            Embalagem mantém o café intacto
+          </div>
+          <div>
+            <Timer className="timer-icon" weight="fill" color="#ffff" /> Entrega rápida e rastreada{" "}
+            <Coffee className="coffe-icon" weight="fill" color="#ffff" /> O café chega fresquinho até você
+          </div>
+        </IconsContainer>
+      </SectionContainer>
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/style.ts
+++ src/style.ts
@@ -15,7 +15,7 @@ export const HeaderContainer = styled.header`
     color: ${(props) => props.theme['purple-dark']};
     padding: 0.5rem;
     border-radius: 8px;
-    font-size: 1.2rem;
+    font-size: 1rem;
     height: 1.5rem;
     font-family: 'Roboto', sans-serif;
     margin-left: auto;
@@ -24,11 +24,103 @@ export const HeaderContainer = styled.header`
   span button {
     cursor: pointer;
     background: ${(props) => props.theme['yellow-light']};
-    margin-top: 1.2rem;
+    margin-top: 1rem;
     padding: 0.5rem;
     border: 0;
     border-radius: 9px;
     margin-right: 7rem;
   }
 
-` 
+  @media (max-width: 768px) {
+    margin-left: 2rem;
+    flex-direction: column;
+
+    span button {
+      margin-right: 0;
+      margin-top: 0.5rem;
+    }
+  }
+`
+
+export const SectionContainer = styled.section`
+  margin-top: 2rem;
+  margin-left: 4.5rem;
+
+  img {
+    width: 38%;
+    margin-left: auto;
+    margin-right: 2rem;
+    margin-top: -14rem;
+  }
+
+  .about-coffee-section {
+    display: flex;
+    flex-direction: column;     
+    font-family: 'Roboto', sans-serif;
+  }
+
+  .about-coffee-section h1 {
+    font-family: 'Baloo 2';
+    font-size: 2.8rem;
+  }
+
+  .about-coffee-section p {
+    margin-top: -1.5rem;
+  }
+
+  @media (max-width: 768px) {
+    margin-left: 2rem;
+
+    img {
+      width: 100%;
+      margin-top: 0;
+      margin-right: 0;
+      margin-bottom: 1.5rem;
+    }
+  }
+`
+
+export const IconsContainer = styled.div`
+  margin-top: -5rem;
+
+  .shoppingCart-icon,
+  .package-icon,
+  .timer-icon,
+  .coffe-icon {
+    background-color: transparent;
+    padding: 0.15rem;
+    border-radius: 999px;
+    margin-right: 7px;
+  }
+
+  .shoppingCart-icon {
+    background: ${(props) => props.theme['yellow-dark']};
+  }
+
+  .package-icon {
+    background: ${(props) => props.theme['base-text']};
+  }
+
+  .timer-icon {
+    background: ${(props) => props.theme['yellow']};
+    margin-top: 1rem;
+  }
+
+  .coffe-icon {
+    background: ${(props) => props.theme['purple']};
+    margin-left: 1rem;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 0;
+
+    .shoppingCart-icon,
+    .package-icon,
+    .timer-icon,
+    .coffe-icon {
+      margin-bottom: 0.5rem;
+    }
+  }
+`
