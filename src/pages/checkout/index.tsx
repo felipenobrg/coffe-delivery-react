60cde19ebe0ebc8d1b3564781b208d83fd791798
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-05-30 17:44:26 -0300

    creating payment styles

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,15 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { Header } from "../../components/Header";
 import { defaultTheme } from "../../styles/theme/default";
-import { Testee } from "./styles";
-import { MapPinLine } from "phosphor-react";
+import { CheckoutContainer } from "./styles";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
 
 export const Checkout = () => (
   <>
     <ThemeProvider theme={defaultTheme}>
       <Header />
-      <Testee>
+      <CheckoutContainer>
         <h2>Complete seu pedido</h2>
+        <div className="main-row">
         <div className="form-container">
           <p> <MapPinLine size={22} color="#C47F17" /> Endereço de Entrega</p>
           <p> Informe o endereço onde receber seu pedido</p>
@@ -28,9 +29,9 @@ export const Checkout = () => (
             </div>
           </form>
         </div>
-
-          <div>
-            <h2>Café selecionados</h2>
+       
+          <div className="coffe-select">
+          <h2>Café selecionados</h2>
             <div>
               <p>Total de itens</p>
               <p>Entrega</p>
@@ -38,18 +39,22 @@ export const Checkout = () => (
               <button>CONFIRMAR PEDIDO</button>
             </div>
           </div>
+          </div>
 
-        <div>
-          <h4>Pagamento</h4>
-          <p>
+        <div className="payment-container">
+          <div className="payment-div">
+          <p><CurrencyDollar size={20} color="#8047F8" weight="fill" /> Pagamento </p>
+          <p className="p-payment-deliver">
             O pagamento é feito na entrega. Escolha a forma que deseja pagar
           </p>
-          <div>
-            <span>CARTÃO DE CRÉDITO</span> <span>CARTÃO DE DÉBITO</span>{" "}
-            <span>DINHEIRO</span>
+          <div className="button-container">
+            <button> <CreditCard size={16} color="#8047F8"/>  CARTÃO DE CRÉDITO</button>
+            <button> <Bank size={16} color="#8047F8"/> CARTÃO DE DÉBITO</button>
+            <button> <Money size={16} color="#8047F8"/> DINHEIRO</button>
           </div>
         </div>
-      </Testee>
+        </div>
+      </CheckoutContainer>
     </ThemeProvider>
   </>
 );
--- src/pages/checkout/styles.ts
+++ src/pages/checkout/styles.ts
@@ -1,16 +1,22 @@
 import styled from "styled-components";
 
-export const Testee = styled.div`
+export const CheckoutContainer = styled.div`
   display: flex;
   flex-direction: column;
   font-family: 'Baloo 2', sans-serif;  
+
+  .main-row {
+    display: flex;
+    gap: 1rem;
+  }
 
   h2 {
     margin-left: 2rem;
   }
 
   .form-container {
-    width: 50%;
+    width: 51%;
+    height: 22rem;
     padding: 2rem;
     margin-left: 3rem;
     font-family: 'Roboto', sans-serif;
@@ -49,5 +55,61 @@ export const Testee = styled.div`
 
   .uf {
     width: 10%;
+  }
+
+  .coffe-select {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: red;
+    width: 35%;
+    height: 30rem;
+    border-radius: 9px;
+    background: ${(props) => props.theme['base-input']};
+  }
+
+  .coffe-select h2 {
+    margin-top: -23rem;
+    margin-right: 8rem;
+  }
+
+  .payment-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-family: 'Roboto', sans-serif;
+    background: ${(props) => props.theme['base-input']};
+    width: 54.7%;
+    height: 11rem;
+    margin-left: 3rem;
+    border-radius: 9px;
+  }
+
+  .payment-div {
+    margin-left: 2rem;
+  }
+
+  .p-payment-deliver {
+    margin-left: 1rem;
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  .button-container {
+    margin-left: 1rem;
+  }
+
+  .button-container button {
+    background: ${(props) => props.theme['base-button']};
+    padding: 0.8rem;
+    border-radius: 9px;
+    border: none;
+    margin-right: 0.3rem;
+    cursor: pointer;
+  }
+
+  .button-container button:hover {
+    background: ${(props) => props.theme['purple-light']};
+    transition: 0.3 ease;
   }
 `
