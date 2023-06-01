82086e809484ff429731e5d211b66208f539f209
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-06-01 14:09:43 -0300

    putting theme provider in the main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/global.ts'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './routes/routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.ts";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./routes/routes.tsx";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default.ts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Router />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
